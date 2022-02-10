cc9c2e1b3ddae4baa1912388e611f68b5e3e298f
IslomAliy <kislombek71@gmail.com>
2022-02-10 11:03:49 +0500

    modal added

--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import styles from './todo.module.scss'
+import React, { useState } from 'react';
+import styles from './todo.module.scss';
+
+
+let maxId = 1;
+
+const createTodo = (label) => {
+    return {
+        label,
+        done: false,
+        id: maxId++
+    }
+
+}
+
+const initialTodo = [
+
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('Add participiants to Magnolia'),
+    createTodo('This is second Todo'),
+    createTodo('This is third Todo'),
+]
 
 const Todo = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const [todos, setTodos] = useState(initialTodo)
+
   return (
+    <>
     <div className={styles.todoWrapper}>
         <div className={styles.headSection}>
             <h1 className={styles.headText}>To do</h1>
@@ -11,94 +44,58 @@ const Todo = () => {
                     <option value="today">Today</option>
                     <option value="tomorrow">Tomorrow</option>
                 </select>
-                <button type='button' className={styles.addBtn}><img src="/images/plus.svg" alt="plus" /></button>
+                <button className={styles.addBtn} onClick={() => setIsOpen(true)} >
+                    <img src="/images/plus.svg" alt="plus" />
+                </button>
+            
             </div>
         </div>
-        <div className={styles.todoArea}>
-            {/* <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                <p> Add participants to Magnolia</p>
-            </div> */}
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
-            <div className={styles.todoElement}>
-                <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
-                <div className={styles.inputWrapper}>
-                    <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
-                    <p> Add participants to Magnolia</p>
-                </div>
-            </div>
+        <div className={styles.todoArea}> 
+            {todos.map((todosData)=> (
+                <div className={styles.todoElement} key={todosData.id}>
+                  <img src="/images/change-order.svg" alt="change-order" className={styles.dragIcon}/>
+                  <div className={styles.inputWrapper}>
+                      <input type="checkbox" name="checkbox" id="checkbox" className={styles.checkbox}/>
+                      <p> {todosData.label}</p>
+                  </div>
+              </div>
+            ))}
         </div>
+        {isOpen && (
+            <div className={styles.overlay}/>
+        )}
+      
     </div>
+    
+    {isOpen && 
+    <div className={styles.modal}>
+            <div className={styles.modalWrapper}>
+                <h1 className={styles.modalHeading}>Add to do list</h1>
+                <form>
+                    <div className={styles.inputForms}>             
+                        <div className={styles.inputField}>
+                            <label htmlFor="title">Title </label>
+                            <input type="text" placeholder='Title'/>
+                        </div>
+                        <div className={styles.inputField}>
+                            <label htmlFor="title">Description </label>
+                            <input type="text" placeholder='Description'/>
+                        </div>
+                        <div className={styles.checkboxField}>
+                            <label htmlFor="status">Status </label>
+                            <input type="checkbox" />
+                        </div>
+                    </div>
+
+                    <div className={styles.buttons}>
+                        <button type='button' className={styles.cancelBtn} onClick={() => setIsOpen(false)}>Cancel</button>
+                        <button type="submit" className={styles.saveBtn}>Save</button>
+                    </div>
+                </form>
+            </div>
+        </div>  
+        }   
+    </>
     );
 };
 
