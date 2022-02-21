1ce78ca5306b63a126b8ae399a8d904c8840e3c5
islomaliy <kislombek71@gmail.com>
2022-02-21 15:10:41 +0500

    home page added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,11 @@
-import Folders from "./components/Folders";
-import Menu from "./components/Menu";
-import Table from "./components/Table";
-import TodoProjectsWrapper from "./components/TodoProjectsWrapper";
-import { db } from "./firebase-config";
-import { storage } from "./firebase-config";
+import React from "react";
+import Folders from "../components/Folders";
+import Menu from "../components/Menu";
+import Table from "../components/Table";
+import TodoProjectsWrapper from "../components/TodoProjectsWrapper";
+
+import { db } from "../firebase-config";
+import { storage } from "../firebase-config";
 import {
   addDoc,
   collection,
@@ -17,12 +19,7 @@ import { useEffect, useState } from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Home from "./pages/Home";
-
-// let timestamp = Math.round(new Date().getTime() / 1000);
-
-function App() {
+const Home = () => {
   let timestamp = Math.round(new Date().getTime() / 1000);
   const [startDate, setStartDate] = useState(Date.now());
   const [endDate, setEndDate] = useState(Date.now());
@@ -31,18 +28,18 @@ function App() {
   const [projectsData, setProjectsData] = useState([]);
   const [newProject, setNewProject] = useState("");
   const projectsCollection = collection(db, "projects");
-  const [folder, setFolder] = useState('')
+  const [folder, setFolder] = useState("");
   const [image, setImage] = useState({
     file: null,
     uploaded: "",
   });
   const [uploadedImage, setUploadedImage] = useState("");
   const [url, setUrl] = useState("");
-  const [isPressed, setIsPressed] = useState(false)
+  const [isPressed, setIsPressed] = useState(false);
 
-  console.log('image = ', image);
+  console.log("image = ", image);
   console.log("url", url);
-  console.log(folder, 'inside app') 
+  console.log(folder, "inside app");
 
   const getProjects = async () => {
     const data = await getDocs(projectsCollection);
@@ -55,11 +52,11 @@ function App() {
           startDate: doc.data().startDate,
           endDate: doc.data().endDate,
           url: doc.data().url,
-          folder: doc.data().folder
+          folder: doc.data().folder,
         }))
         .sort(function (a, b) {
           return a.timeStamp - b.timeStamp;
-        })      
+        })
         .slice(-3)
         .reverse()
     );
@@ -103,7 +100,7 @@ function App() {
         .then((url) => {
           console.log(url);
           setUrl(url);
-          func(prop)
+          func(prop);
         })
         .catch((error) => {
           console.log(error.message);
@@ -112,33 +109,33 @@ function App() {
   };
 
   const addProject = async () => {
-    console.log('addURl', url)
+    console.log("addURl", url);
 
     await addDoc(projectsCollection, {
-        label: newProject,
-        timeStamp: timestamp,
-        startDate: startDate.toLocaleDateString(),
-        endDate: endDate.toLocaleDateString(),
-        url: url,
-        folder: folder
-      });
-      getProjects();
-      setIsPressed(false)
-      setNewProject("");
-      setIsOpen(false);
-      setFolder('')
-      setUrl("");
-      setImage({
-        file: null,
-        uploaded: "",
-      });
+      label: newProject,
+      timeStamp: timestamp,
+      startDate: startDate.toLocaleDateString(),
+      endDate: endDate.toLocaleDateString(),
+      url: url,
+      folder: folder,
+    });
+    getProjects();
+    setIsPressed(false);
+    setNewProject("");
+    setIsOpen(false);
+    setFolder("");
+    setUrl("");
+    setImage({
+      file: null,
+      uploaded: "",
+    });
   };
 
   useEffect(() => {
     getProjects();
   }, []);
 
-  console.log('projectsData',projectsData);
+  console.log("projectsData", projectsData);
 
   const deleteProject = async (id) => {
     const projectDoc = doc(db, "projects", id);
@@ -146,15 +143,9 @@ function App() {
     getProjects();
     console.log(id);
   };
-
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home/>}/> 
-        </Routes>
-      </Router>
-      {/* <Menu />
+    <div>
+      <Menu />
       <Folders
         addProject={addProject}
         isOpen={isOpen}
@@ -186,11 +177,10 @@ function App() {
         setEndDate={setEndDate}
         image={image}
         setImage={setImage}
-
       />
-      <Table /> */}
+      <Table />
     </div>
   );
-}
+};
 
-export default App;
+export default Home;
