249ae55c67e16d9a7e20b54d9a69ac85c928b12c
islomaliy <kislombek71@gmail.com>
2022-02-23 12:06:24 +0500

    skeleton added to my projects

--- src/components/MyProjects/index.jsx
+++ src/components/MyProjects/index.jsx
@@ -1,10 +1,16 @@
-import './style.module.scss'
+import "./style.module.scss";
 import { useState, useEffect } from "react";
 import Layout from "../Layout";
 import Menu from "../Menu";
 import styles from "./style.module.scss";
 import { db, storage } from "../../firebase-config";
-import { collection, getDocs, doc, deleteDoc, updateDoc} from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  doc,
+  deleteDoc,
+  updateDoc,
+} from "firebase/firestore";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
@@ -14,24 +20,31 @@ import "react-datepicker/dist/react-datepicker.css";
 const MyProjects = () => {
   let timestamp = Math.round(new Date().getTime() / 1000);
   const [projectsData, setProjectsData] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [isEditOpen, setIsEditOpen] = useState(false);
   const [isPressed, setIsPressed] = useState(false);
-  const [label, setLabel] = useState('')
+  const [label, setLabel] = useState("");
   const [startDate, setStartDate] = useState(Date.now());
   const [endDate, setEndDate] = useState(Date.now());
   const [folder, setFolder] = useState("");
-  const [url, setUrl] = useState('')
-  const [projectId, setProjectId] = useState('')
+  const [url, setUrl] = useState("");
+  const [projectId, setProjectId] = useState("");
   const [image, setImage] = useState({
     file: null,
     uploaded: "",
   });
   const projectsCollection = collection(db, "projects");
 
+  useEffect(() => {
+    const loading = setTimeout(() => {
+      setIsLoading(false);
+    }, 3500);
+
+    return () => clearTimeout(loading);
+  }, []);
 
   const getProjects = async () => {
-    setIsLoading(true)
+    setIsLoading(true);
     const data = await getDocs(projectsCollection);
     setProjectsData(
       data.docs
@@ -45,20 +58,19 @@ const MyProjects = () => {
           folder: doc.data().folder,
         }))
         .sort(function (a, b) {
-            return a.timeStamp - b.timeStamp;
-          })
+          return a.timeStamp - b.timeStamp;
+        })
         .reverse()
-    )
-    setIsLoading(false)
+    );
+    setIsLoading(false);
   };
 
-//   console.log('testData' + projectsData.map((data) => data))
-
+  //   console.log('testData' + projectsData.map((data) => data))
 
   const handleEdit = (id) => {
     setProjectId(id);
-    setIsEditOpen(true)
-  }
+    setIsEditOpen(true);
+  };
 
   const deleteProject = async (id) => {
     const projectDoc = doc(db, "projects", id);
@@ -71,7 +83,6 @@ const MyProjects = () => {
     getProjects();
   }, []);
 
-
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsPressed(true);
@@ -80,7 +91,7 @@ const MyProjects = () => {
     setImage({
       file: null,
       uploaded: "",
-    }); 
+    });
     const projectDoc = doc(db, "projects", projectId);
 
     updateDoc(projectDoc, {
@@ -88,19 +99,18 @@ const MyProjects = () => {
       startDate: startDate.toLocaleDateString(),
       endDate: endDate.toLocaleDateString(),
       url: url,
-      folder: folder
-    })
+      folder: folder,
+    });
   };
 
   const handleClickOverlay = () => {
     setIsEditOpen(false);
-    setLabel('')
+    setLabel("");
     setImage({
       file: null,
       uploaded: "",
     });
   };
-
 
   const changeSelectValue = (newValue) => {
     setFolder(newValue);
@@ -119,7 +129,7 @@ const MyProjects = () => {
     setFolder("");
     setIsEditOpen(false);
     setIsPressed(false);
-    setLabel('')
+    setLabel("");
     setImage({
       file: null,
       uploaded: "",
@@ -145,145 +155,213 @@ const MyProjects = () => {
       <Layout>
         <Menu />
         <div className={styles.favouritesWrapper}>
-          <h2 className={styles.favouritesHeading}>All projects show up there.</h2>
+          <h2 className={styles.favouritesHeading}>
+            All projects show up there.
+          </h2>
           <div className={styles.projectsCardWrapper}>
-            {projectsData.map((projectData) => (
-              <div className={styles.projectsCard} key={projectsData.timeStamp}>
-                <div className={styles.leftSide}>
-                  <img
-                    src={projectData.url}
-                    alt="project-img"
-                    className={styles.projectsImg}
-                  />
-                  <div className={styles.projectsName}>
-                    <p className={styles.projectsText}>
-                      {isLoading ? <Skeleton width={70} /> : projectData.label}
-                    </p>
-                    <div className={styles.projectsDate}>
-                      <img src="/images/calendar_today.svg" alt="calendar" />
-                      <p className={styles.date}>
-                        {projectData.startDate} - {projectData.endDate}
-                      </p>
-                    </div>
-                  </div>
-                </div>
-                <div className={styles.rightSide}>
-                  <button
-                    type="button"
-                    className={styles.editBtn}
-                    onClick={() => handleEdit(projectData.id)}
-                  >
-                    Edit
-                  </button>
-                  <button
-                    className={styles.deleteBtn}
-                    onClick={() => {
-                      deleteProject(projectData.id);
-                    }}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-            ))}
-          </div>
-          {isEditOpen && (
-        <>
-          <div className={styles.overlay} onClick={handleClickOverlay} />
-          <div className={styles.modal}>
-            <div className={styles.modalWrapper}>
-              <h1 className={styles.modalHeading}>Updating project</h1>
-              <form className={styles.modalForm} onSubmit={handleSubmit}>
-                <div className={styles.inputForms}>
-                  <div className={styles.inputField}>
-                    <label htmlFor="title">Title of project </label>
-                    <input
-                      type="text"
-                      placeholder="Title of project"
-                      value={label}
-                      onChange={(e) => setLabel(e.target.value)}
-                      required
+            {isLoading && (
+              <>
+                <div className={styles.projectsCard}>
+                  <div className={styles.leftSide}>
+                    <Skeleton
+                      width={50}
+                      height={40}
+                      style={{ marginRight: "30px" }}
                     />
-                  </div>
-                  <div className={styles.dateField}>
-                    <label htmlFor="title">Date </label>
-                    <div className={styles.datesWrapper}>
-                      <DatePicker
-                        className={styles.DatePicker}
-                        selected={startDate}
-                        onChange={(date) => setStartDate(date)}
-                        selectsStart
-                        startDate={startDate}
-                        endDate={endDate}
-                        required
-                      />
-                      <DatePicker
-                        className={styles.DatePicker}
-                        selected={endDate}
-                        onChange={(date) => setEndDate(date)}
-                        selectsEnd
-                        startDate={startDate}
-                        endDate={endDate}
-                        minDate={startDate}
-                        required
-                      />
+                    <div className={styles.projectsName}>
+                      <p className={styles.projectsText}>
+                        {<Skeleton width={400} />}
+                      </p>
+                      <div className={styles.projectsDate}>
+                        <p className={styles.date}>{<Skeleton width={60} />}</p>
+                      </div>
                     </div>
-                  </div>
-                  <div className={styles.fileUploadField}>
-                    <label htmlFor="status">Image </label>
-                    {image.file == null && (
-                      <input
-                        type="file"
-                        accept=".jpg, .png, .jpeg"
-                        onChange={handleImageChange}
-                        required
-                      />
-                    )}
-                    {/* style={`${image.file ? display: 'none' : ''}`} */}
-                    {image.file !== null && (
-                      <img src={image.file} alt="uploaded-picture" />
-                    )}
-                  </div>
-                  <div className={styles.membersField}>
-                    <label htmlFor="title">Members </label>
-                    <select
-                      onChange={(e) => changeSelectValue(e.target.value)}
-                      value={folder}
-                      required
-                    >
-                      <option value="" defaultValue disabled hidden>
-                        {" "}
-                        Select folder{" "}
-                      </option>
-                      <option value="favourites">Favourites</option>
-                      <option value="trips">Trips</option>
-                      <option value="work">Work</option>
-                    </select>
                   </div>
                 </div>
 
-                <div className={styles.buttons}>
-                  <button
-                    type="button"
-                    className={styles.cancelBtn}
-                    onClick={() => cancelBtn()}
-                  >
-                    Cancel
-                  </button>
-                  <button
-                    type="submit"
-                    className={styles.saveBtn}
-                    disabled={isPressed}
-                    onClick={() => console.log("pressed")}
-                  >
-                    {isPressed ? "Loading" : "Save"}
-                  </button>
+                <div className={styles.projectsCard}>
+                  <div className={styles.leftSide}>
+                    <Skeleton
+                      width={50}
+                      height={40}
+                      style={{ marginRight: "30px" }}
+                    />
+                    <div className={styles.projectsName}>
+                      <p className={styles.projectsText}>
+                        {<Skeleton width={400} />}
+                      </p>
+                      <div className={styles.projectsDate}>
+                        <p className={styles.date}>{<Skeleton width={60} />}</p>
+                      </div>
+                    </div>
+                  </div>
                 </div>
-              </form>
-            </div>
+
+                <div className={styles.projectsCard}>
+                  <div className={styles.leftSide}>
+                    <Skeleton
+                      width={50}
+                      height={40}
+                      style={{ marginRight: "30px" }}
+                    />
+                    <div className={styles.projectsName}>
+                      <p className={styles.projectsText}>
+                        {<Skeleton width={400} />}
+                      </p>
+                      <div className={styles.projectsDate}>
+                        <p className={styles.date}>{<Skeleton width={60} />}</p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )}
+
+            {!isLoading &&
+              projectsData.map((projectData) => (
+                <div
+                  className={styles.projectsCard}
+                  key={projectsData.timeStamp}
+                >
+                  <div className={styles.leftSide}>
+                    <img
+                      src={projectData.url}
+                      alt="project-img"
+                      className={styles.projectsImg}
+                    />
+                    <div className={styles.projectsName}>
+                      <p className={styles.projectsText}>
+                        {isLoading ? (
+                          <Skeleton width={70} />
+                        ) : (
+                          projectData.label
+                        )}
+                      </p>
+                      <div className={styles.projectsDate}>
+                        <img src="/images/calendar_today.svg" alt="calendar" />
+                        <p className={styles.date}>
+                          {projectData.startDate} - {projectData.endDate}
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                  <div className={styles.rightSide}>
+                    <button
+                      type="button"
+                      className={styles.editBtn}
+                      onClick={() => handleEdit(projectData.id)}
+                    >
+                      Edit
+                    </button>
+                    <button
+                      className={styles.deleteBtn}
+                      onClick={() => {
+                        deleteProject(projectData.id);
+                      }}
+                    >
+                      Delete
+                    </button>
+                  </div>
+                </div>
+              ))}
           </div>
-        </>
-      )}
+          {isEditOpen && (
+            <>
+              <div className={styles.overlay} onClick={handleClickOverlay} />
+              <div className={styles.modal}>
+                <div className={styles.modalWrapper}>
+                  <h1 className={styles.modalHeading}>Updating project</h1>
+                  <form className={styles.modalForm} onSubmit={handleSubmit}>
+                    <div className={styles.inputForms}>
+                      <div className={styles.inputField}>
+                        <label htmlFor="title">Title of project </label>
+                        <input
+                          type="text"
+                          placeholder="Title of project"
+                          value={label}
+                          onChange={(e) => setLabel(e.target.value)}
+                          required
+                        />
+                      </div>
+                      <div className={styles.dateField}>
+                        <label htmlFor="title">Date </label>
+                        <div className={styles.datesWrapper}>
+                          <DatePicker
+                            className={styles.DatePicker}
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            selectsStart
+                            startDate={startDate}
+                            endDate={endDate}
+                            required
+                          />
+                          <DatePicker
+                            className={styles.DatePicker}
+                            selected={endDate}
+                            onChange={(date) => setEndDate(date)}
+                            selectsEnd
+                            startDate={startDate}
+                            endDate={endDate}
+                            minDate={startDate}
+                            required
+                          />
+                        </div>
+                      </div>
+                      <div className={styles.fileUploadField}>
+                        <label htmlFor="status">Image </label>
+                        {image.file == null && (
+                          <input
+                            type="file"
+                            accept=".jpg, .png, .jpeg"
+                            onChange={handleImageChange}
+                            required
+                          />
+                        )}
+                        {/* style={`${image.file ? display: 'none' : ''}`} */}
+                        {image.file !== null && (
+                          <img src={image.file} alt="uploaded-picture" />
+                        )}
+                      </div>
+                      <div className={styles.membersField}>
+                        <label htmlFor="title">Members </label>
+                        <select
+                          onChange={(e) => changeSelectValue(e.target.value)}
+                          value={folder}
+                          required
+                        >
+                          <option value="" defaultValue disabled hidden>
+                            {" "}
+                            Select folder{" "}
+                          </option>
+                          <option value="favourites">Favourites</option>
+                          <option value="trips">Trips</option>
+                          <option value="work">Work</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className={styles.buttons}>
+                      <button
+                        type="button"
+                        className={styles.cancelBtn}
+                        onClick={() => cancelBtn()}
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        type="submit"
+                        className={styles.saveBtn}
+                        disabled={isPressed}
+                        onClick={() => console.log("pressed")}
+                      >
+                        {isPressed ? "Loading" : "Save"}
+                      </button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </Layout>
     </>
